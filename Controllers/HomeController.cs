931ddd98cf07eb596b949897b06a2d18bb5fae16
Charlie Moreau <moreaucharlie25@gmail.com>
2020-02-21 09:31:31 +0100

    suppression des pages about et contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,18 +19,5 @@ namespace AfpEat.Controllers
             return View(db.TypeCuisines.ToList());
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }

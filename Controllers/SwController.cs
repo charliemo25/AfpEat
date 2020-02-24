09c5b66325b88b04898e0389565fe54dce75c520
Charlie Moreau <moreaucharlie25@gmail.com>
2020-02-24 10:42:49 +0100

    ajout d'un Sw controller

---
+++ Controllers/SwController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AfpEat.Controllers
+{
+    public class SwController : Controller
+    {
+        [HttpGet]
+        public JsonResult AddProduit(int idProduit, string idSession)
+        {
+            List<int> panier = new List<int>();
+            panier.Add(idProduit);
+            HttpContext.Application[idSession] = "";
+            return Json("toto", JsonRequestBehavior.AllowGet);
+        }
+    }
+}

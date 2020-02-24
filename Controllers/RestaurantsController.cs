ce8b1ec55e88e3388ac29a677326496941a58c88
Charlie Moreau <moreaucharlie25@gmail.com>
2020-02-24 10:19:52 +0100

    tri par groupby fonctionnel

--- Controllers/RestaurantsController.cs
+++ Controllers/RestaurantsController.cs
@@ -34,6 +34,8 @@ namespace AfpEat.Controllers
                 return HttpNotFound();
             }
 
+            var test = restaurant.ProduitCategories.GroupBy(p => p.IdCategorie).ToList();
+
             return View(restaurant);
         }
 

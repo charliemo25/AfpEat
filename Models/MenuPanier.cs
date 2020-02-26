4a84fbf0921f5b3fc8144d0ad16ea96c6f43b70a
Charlie Moreau <moreaucharlie25@gmail.com>
2020-02-26 12:01:39 +0100

    list id produit dans MenuPanier

--- Models/MenuPanier.cs
+++ Models/MenuPanier.cs
@@ -8,6 +8,7 @@ namespace AfpEat.Models
     public class MenuPanier
     {
         public int IdMenu { get; set; }
+        public List<int> IdProduit { get; set; }
         public int IdRestaurant { get; set; }
         public string Nom { get; set; }
         public decimal Prix { get; set; }

2c9ebcb208105d9a9bf5c87c8ed0ad5f20610ae2
Charlie Moreau <moreaucharlie25@gmail.com>
2020-03-03 10:20:44 +0100

    alert sur detailsCommande

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -21,7 +21,7 @@ namespace AfpEat
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-notify.min.js"));
+                      "~/Scripts/bootstrap-notify/bootstrap-notify.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
